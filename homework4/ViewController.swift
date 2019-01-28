99b892a70905afceecbd15630f9a703863e14e5c
piglier <pig-2000-b@hotmail.com>
2019-01-29 00:38:04 +0800

    Initial Commit

---
+++ homework4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework4
+//
+//  Created by PIG on 2019/1/29.
+//  Copyright © 2019 PIG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
