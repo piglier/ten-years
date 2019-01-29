da26f88efddcfc9ac0197cfdc493f67e6d7bebe9
piglier <pig-2000-b@hotmail.com>
2019-01-30 01:49:11 +0800

    ten years

--- homework4/ViewController.swift
+++ homework4/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var YearSlider: UISlider!
+    @IBOutlet weak var ChooseImg: UIImageView!
+    
+    @IBOutlet weak var DatePicker: UIDatePicker!
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    @IBOutlet weak var Label2: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        Label.text = String(Int(YearSlider.value))
+        let dateValue = DateFormatter()
+        dateValue.dateFormat = "YYYY"
+        let y = dateValue.string(from: DatePicker.date)
+        Label2.text = String(y)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func YearSliderAction(_ sender: Any) {
+        ChooseImg.image = UIImage(named: "\(Int(YearSlider.value)).png")
+        Label.text = String(Int(YearSlider.value))
+        
+    }
+    
+    @IBAction func DatePickerAction(_ sender: Any) {
+        let dateValue = DateFormatter()
+        dateValue.dateFormat = "YYYY"
+        let y = dateValue.string(from: DatePicker.date)
+        let yy = Int(y)
+        let year = yy!-1911
+        ChooseImg.image = UIImage(named: "\(Int(Int(year))).png")
+        Label2.text = String(y)
+        
+    }
 }
 
